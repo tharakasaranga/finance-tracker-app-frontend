df457e3c5fbf38abfbd081929700c1bc658b418f
Tharaka Saranga <tharakasaranga755@gmail.com>
2026-04-25 10:24:28 +0530

    Vercel Error

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,19 +1,55 @@
 import axios from "axios";
+import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase/firebaseConfig";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
 
+const waitForCurrentUser = () => {
+  if (auth.currentUser) {
+    return Promise.resolve(auth.currentUser);
+  }
+
+  return new Promise((resolve) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      unsubscribe();
+      resolve(user);
+    });
+  });
+};
+
 api.interceptors.request.use(async (config) => {
-  const user = auth.currentUser;
+  const user = await waitForCurrentUser();
 
   if (user) {
     const token = await user.getIdToken();
+    config.headers = config.headers ?? {};
     config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && originalRequest && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      if (auth.currentUser) {
+        const token = await auth.currentUser.getIdToken(true);
+        originalRequest.headers = originalRequest.headers ?? {};
+        originalRequest.headers.Authorization = `Bearer ${token}`;
+
+        return api(originalRequest);
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default api;
